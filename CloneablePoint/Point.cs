f96c944c77ba4af458f64d70d77bc931ff264ea3
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-03-22 18:54:45 +0100

    312

---
+++ CloneablePoint/Point.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    public class Point : ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public PointDescription desc = new PointDescription();
+
+        public Point(int xPos, int yPos, string petName)
+        {
+            X = xPos;
+            Y = yPos;
+            desc.PetName = petName;
+        }
+
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+        }
+
+        public Point()
+        {
+        }
+
+        // overrde Object.ToString()
+        public override string ToString()
+        {
+            return string.Format("X = {0}; Y={1}; Name = {2};\nID = {3}\n", X, Y, desc.PetName, desc.PointID);
+        }
+
+        public object Clone()
+        {
+            //return new Point(this.X, this.Y);
+            //return this.MemberwiseClone();
+
+            // first a shallow copy
+            Point newPoint = (Point) this.MemberwiseClone();
+
+            // then fill in the gaps
+            PointDescription currentDesc = new PointDescription();
+            currentDesc.PetName = this.desc.PetName;
+            newPoint.desc = currentDesc;
+            return newPoint;
+        }
+
+
+    }
+}
